6e36506771ba2931acf2ec9b81e7d11569fc44fc
Hrishekessa Raghu Eeti <eehraghu@gmail.com>
2018-09-13 00:02:13 -0500

    Update Script2Split.py

--- working_with_files/Script2Split.py
+++ working_with_files/Script2Split.py
@@ -4,7 +4,7 @@ this script generates 4 text files for when the text file "inp.txt" is given as 
 # opening the file
 with open('inp.txt') as fo:
 # reading the file
-	fs=fo.read().split("\n") # output is list with number of rows as elements and every element in the list is data in a specific row
+	fs=fo.read().split("\n") # output is list with number of rows as elements and every element in the list is data in a specific row of inp.txt file
 	op=''
 	start=0
 	counter=1 # to name the files generated seperately
@@ -17,7 +17,7 @@ with open('inp.txt') as fo:
 					op='' # clear the op to make it ready for next section
 					counter+=1
 			else:
-				start=1
+				start=1 # when start ==0 the control comes here. the we set the start =1 and this is like giving an instruction to start reading the content . But if already started i.e. start !=0 then save the previous content and get ready for the next content.
 		else:
 			if op=='' : # handling the very first line to avoid blank space at the start of each generated file.
 				op = x
