6d818edbf637d854cf69756f95f60ecc3ed467e3
Hrishekessa Raghu Eeti <eehraghu@gmail.com>
2018-10-14 01:31:03 -0500

    commit pythonreg.py
    added comments

--- regex/sample_regex cases/pythonreg.py
+++ regex/sample_regex cases/pythonreg.py
@@ -7,8 +7,11 @@ Address            Age         Hardware Addr          State        Type      Int
 78.45.89.90      00.44.05     d4:6d:80:f1:b0:b2       Dynamic      ARPA      Gi1/0/1
 68.66.72.97      00:07:12     d4:c6:50:49:42:84       Interface    ARPA      Gi2/1/4
 '''
+#grouping the required elements in this script ip address, mac address and interface are the three groups
 pattern = re.compile('((?:(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])).*((?:[a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}).*([a-zA-Z]{2}[1-9]\/[0-9]\/[0-9])')
+#creating table
 table=PrettyTable(["ip address","mac address","Interface"])
+#matches is an object which we can iterate through
 matches = pattern.finditer(sentence)
 for x in matches:
     table.add_row([x.group(1),x.group(2),x.group(3)])
